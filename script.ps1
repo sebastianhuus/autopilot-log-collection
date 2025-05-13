63e3cab58bc48ec2c6de9d67e98c01b130139891
Sebastian Huus <54155988+sebastianhuus@users.noreply.github.com>
2025-05-13 13:35:51 +0200

    get serial number

--- script.ps1
+++ script.ps1
@@ -7,4 +7,5 @@ mdmdiagnosticstool.exe -area Autopilot; DeviceEnrollment;DeviceProvisioning -cab
 wget https://aka.ms/intunexml -outfile Intune.xml
 wget https://aka.ms/intuneps1 -outfile IntuneODCStandAlone.ps1
 powerShell -ExecutionPolicy Bypass -File .\IntuneODCStandAlone.ps1
+$DeviceSerialNumber = (Get-CimInstance Win32_BIOS).SerialNumber
 
