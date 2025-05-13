cc7e5d58b46a34b7f1c96426c0a1161ecf596793
Sebastian Huus <54155988+sebastianhuus@users.noreply.github.com>
2025-05-13 13:31:47 +0200

    ask for device username

--- script.ps1
+++ script.ps1
@@ -1,8 +1,10 @@
 cd C:/.
 md temp
 cd temp
+$DeviceUsername = Read-Host "Please enter the device username"
 mdmdiagnosticstool.exe -area Autopilot; DeviceEnrollment;DeviceProvisioning -cab autopilot-logs.cab
 
 wget https://aka.ms/intunexml -outfile Intune.xml
 wget https://aka.ms/intuneps1 -outfile IntuneODCStandAlone.ps1
 powerShell -ExecutionPolicy Bypass -File .\IntuneODCStandAlone.ps1
+
