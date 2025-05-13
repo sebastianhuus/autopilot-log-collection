89609539eb1f034dfa5f35c4e9b40d39f0fe684f
Sebastian Huus <54155988+sebastianhuus@users.noreply.github.com>
2025-05-13 13:36:55 +0200

    make a zip archive

--- script.ps1
+++ script.ps1
@@ -8,4 +8,5 @@ wget https://aka.ms/intunexml -outfile Intune.xml
 wget https://aka.ms/intuneps1 -outfile IntuneODCStandAlone.ps1
 powerShell -ExecutionPolicy Bypass -File .\IntuneODCStandAlone.ps1
 $DeviceSerialNumber = (Get-CimInstance Win32_BIOS).SerialNumber
+Compress-Archive -Path * -DestinationPath "..\$($DeviceUsername) $($DeviceSerialNumber).zip" -Force
 
