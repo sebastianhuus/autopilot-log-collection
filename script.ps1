5d1709510c085c3c176322c52e7d5420f7ff73e1
Sebastian Huus <54155988+sebastianhuus@users.noreply.github.com>
2025-05-13 13:28:46 +0200

    make script

---
+++ script.ps1
@@ -0,0 +1,8 @@
+cd C:/.
+md temp
+cd temp
+mdmdiagnosticstool.exe -area Autopilot; DeviceEnrollment;DeviceProvisioning -cab autopilot-logs.cab
+
+wget https://aka.ms/intunexml -outfile Intune.xml
+wget https://aka.ms/intuneps1 -outfile IntuneODCStandAlone.ps1
+powerShell -ExecutionPolicy Bypass -File .\IntuneODCStandAlone.ps1
