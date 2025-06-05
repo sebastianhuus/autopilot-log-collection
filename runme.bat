233e87240186e76709c717367119a53db33af087
Sebastian Huus <sebastian.huuset@gmail.com>
2025-06-05 11:02:25 +0200

    remove admin rights request from the log collector

--- runme.bat
+++ runme.bat
@@ -1,11 +1,1 @@
-@echo off
-REM Check for administrative privileges and relaunch if needed
-NET SESSION >nul 2>&1
-IF %ERRORLEVEL% NEQ 0 (
-    echo Requesting administrative privileges...
-    powershell.exe -Command "Start-Process -FilePath '%~dpnx0' -Verb RunAs"
-    exit /b
-)
-
-REM If we get here, we have admin rights
 powershell.exe -NoExit -ExecutionPolicy Bypass -File "%~dp0\script.ps1"
